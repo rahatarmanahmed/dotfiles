9fbd93f18b8e878b4cbcf64143110a308d6c2fe2
Rahat Ahmed <rahatarmanahmed@gmail.com>
2017-06-03 17:10:06 -0700

    More awesomewm config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -206,10 +206,10 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
+            -- mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
-            s.mylayoutbox,
+            -- s.mylayoutbox,
         },
     }
 end)
@@ -459,7 +459,7 @@ awful.rules.rules = {
 
     -- Add titlebars to normal clients and dialogs
     { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
+      }, properties = { titlebars_enabled = false }
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
