424bcb06aba993074019f8a28283efbc93b8082b
Rahat Ahmed <rahatarmanahmed@gmail.com>
2024-05-17 08:53:30 -0500

    Make enter/tab on autocompelte work

--- .config/nvim/after/plugin/lspzero.lua
+++ .config/nvim/after/plugin/lspzero.lua
@@ -27,3 +27,16 @@ require("mason-lspconfig").setup_handlers {
     -- end
 }
 
+
+local cmp = require('cmp')
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<Tab>'] = cmp.mapping.confirm({select = true}),
+
+    -- scroll up and down the documentation window
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  }),
+})
