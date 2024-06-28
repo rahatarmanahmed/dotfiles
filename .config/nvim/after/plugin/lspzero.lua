fb9f15d7ab7e0d5c23a9ca9579e88fe3d28389fa
Rahat Ahmed <rahatarmanahmed@gmail.com>
2024-06-28 17:46:40 -0500

    Fixed hledger complete

--- .config/nvim/after/plugin/lspzero.lua
+++ .config/nvim/after/plugin/lspzero.lua
@@ -43,22 +43,19 @@ require("mason-lspconfig").setup_handlers {
 }
 
 require('lsp-zero').extend_cmp()
+
 local cmp = require('cmp')
 local cmp_action = lsp_zero.cmp_action()
+
 cmp.setup({
     sources = {
         { name = 'nvim_lsp' },
         { name = 'hledger' },
     },
-  mapping = cmp.mapping.preset.insert({
-    -- confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
-    ['<Tab>'] = cmp.mapping.confirm({select = true}),
-
-    -- scroll up and down the documentation window
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  }),
+    mapping = cmp.mapping.preset.insert({
+        ['<Enter>'] = cmp.mapping.confirm(),
+        ['<Tab>'] = cmp.mapping.confirm(),
+    }),
     -- preselect first item
     preselect = 'item',
     completion = {
