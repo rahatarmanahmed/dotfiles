e4d785a0c00a3c9675d4ce44e839f376973a06ce
Rahat Ahmed <rahatarmanahmed@gmail.com>
2025-05-04 01:18:06 -0500

    remove log out option for now

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SELECTION="$(printf "󰌾 Lock\0󰤄 Sleep\0󰜗 Hibernate\0󰗽 Log Out $USER…\0 Restart…\0󰐥 Shut Down…" | fuzzel --dmenu0)"
+SELECTION="$(printf "󰌾 Lock\0󰤄 Sleep\0󰜗 Hibernate\0 Restart…\0󰐥 Shut Down…" | fuzzel --dmenu0 --anchor=top-left -w 20 -x 5 -y 5 -l 5)"
 
 case "$SELECTION" in
 	*"Lock")
@@ -9,8 +9,6 @@ case "$SELECTION" in
         systemctl suspend ;;
 	*"Hibernate")
         systemctl hibernate;;
-	*"Log Out…")
-        hyprctl dispatch exit;;
 	*"Restart…")
         systemctl reboot;;
 	*"Shut Down…")
