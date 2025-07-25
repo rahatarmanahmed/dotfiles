f310022c14c7dbe7acb04a8c3ed25e67e8aa6cf0
Rahat Ahmed <rahatarmanahmed@gmail.com>
2025-07-24 23:59:35 -0500

    fix desktop entries for jrnl/note
    it didn't like the /home/rahat in the path

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SELECTION="$(printf "󰌾 Lock\0󰤄 Sleep\0󰜗 Hibernate\0 Restart…\0󰐥 Shut Down…" | fuzzel --dmenu0 --anchor=top-left -w 20 -x 5 -y 5 -l 5)"
+SELECTION="$(printf "󰌾 Lock\n󰤄 Sleep\n󰜗 Hibernate\n Restart…\n󰐥 Shut Down…" | tofi)"
 
 case "$SELECTION" in
 	*"Lock")
